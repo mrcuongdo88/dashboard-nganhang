52a76892390a6b8fd8f5781c6e63e57081c79bbf
mrcuongdo88 <mrcuongdo88@gmail.com>
2026-05-18 14:41:09 +0700

    update dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1480,7 +1480,13 @@ async function updateNextAction(
 
   e.stopPropagation()
 
-  openTimeline(item.id)
+  setSelectedApplicationId(
+    item.id
+  )
+
+  fetchTimeline(item.id)
+
+  setShowTimeline(true)
 }}
                           className="bg-indigo-500 text-white px-4 py-2 rounded-xl hover:bg-indigo-600"
                         >
